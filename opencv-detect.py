8007289f959469207afd1f07f5e0be66bacac5bb
Philip Zhang <zhangxilai@doublechaintech.com>
2021-03-04 02:33:05 +0800

    Add open cv script

--- opencv-detect.py
+++ opencv-detect.py
@@ -5,33 +5,30 @@ img = cv2.imread('images/004.png')
 imgGry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 ret , thrash = cv2.threshold(imgGry, 240 , 255, cv2.CHAIN_APPROX_NONE)
-contours , hierarchy = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+_, contours , hierarchy = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-
+counter=0
 
 for contour in contours:
     approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
     cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
     x = approx.ravel()[0]
     y = approx.ravel()[1] - 5
-    if len(approx) == 3:
+    if len(approx) == 1000:
         cv2.putText( img, "Triangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0) )
     elif len(approx) == 4 :
         x, y , w, h = cv2.boundingRect(approx)
         aspectRatio = float(w)/h
-        print(aspectRatio)
+        #print(aspectRatio)
+        if h>60:
+            print("values ", x, y,w,h )
         if aspectRatio >= 0.95 and aspectRatio < 1.05:
             cv2.putText(img, "square", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
 
         else:
-            cv2.putText(img, "rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+            cv2.putText(img, "rectangle {:.2f}".format(counter), (x+10, y+20), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+            counter=counter+1
 
-    elif len(approx) == 5 :
-        cv2.putText(img, "pentagon", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-    elif len(approx) == 10 :
-        cv2.putText(img, "star", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-    else:
-        cv2.putText(img, "circle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
 
 #cv2.imshow('shapes', img)
 cv2.imwrite('/var/www/html/upload/rect.png', img)
