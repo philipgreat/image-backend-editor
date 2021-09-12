0671099007a0d1448f2a57c238b3ee8ee2504474
Philip Zhang <zhangxilai@doublechaintech.com>
2021-09-12 21:47:03 +0800

    Split style and html

--- find-blank-input.py
+++ find-blank-input.py
@@ -22,6 +22,10 @@ sortedContours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0] + cv2
 
 counter=0
 
+style=""
+html=""
+
+
 for contour in sortedContours:
     approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
     #cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
@@ -47,13 +51,22 @@ for contour in sortedContours:
             if color_count==0:
                 #print("values-{:.0f} ".format(counter),"left: ","{:.02f}px;".format(x*ratio), "top: ","{:.02f}px;".format(y*ratio), "width: ","{:.02f}px;".format(w*ratio-5), "height: ","{:.02f}px;".format(h*ratio) )
                 
+                #parameters=(left=x*ratio-2,top=y*ratio-2,width=w*ratio+4,height=h*ratio+4,cellNumber=counter);
                 template='<div style="position: absolute;overflow: hidden;padding: 0px;left:{left}px; top:{top}px; width:{width}px; height:{height}px;"></div>';
                 print(template.format(left=x*ratio-2,top=y*ratio-2,width=w*ratio+4,height=h*ratio+4,cellNumber=counter))
-                
+                styletemplate='position: absolute;overflow: hidden;padding: 0px;left:{left}px; top:{top}px; width:{width}px; height:{height}px;'
+                style=style+styletemplate.format(left=x*ratio-2,top=y*ratio-2,width=w*ratio+4,height=h*ratio+4,cellNumber=counter)
+                html=html+'<div id="{counter}"> </div>'.format(counter=counter)
+
+
                 
                 counter=counter+1
                 cv2.putText(img, "rectangle {:.0f}".format(counter), (x+10, y+20), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 255))
     
+
+print(style)
+print(html)
+
 
 #cv2.imshow('shapes', img)
 #cv2.imwrite('/var/www/html/upload/rect.png', img)
